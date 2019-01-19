e1985e758111a89162fdc71251d52d0e1bf38a7b
unknown <unknown@example.com>
2019-01-19 12:22:52 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,39 @@
+//app.js
+App({
+  onLaunch: function () {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+  },
+  getUserInfo:function(cb){
+    var that = this
+    if(this.globalData.userInfo){
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    }else{
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },
+  globalData:{
+    userInfo:null
+  }
+})
+
+//初始化LeanCloud对象
+const AV = require('./libs/av-weapp.js');
+AV.init({
+  //appId: 'fYrPgAK53LHnhNxkmvuCDNtp-gzGzoHsz',
+  appId: 'CWjfwdlPahMF3JhRasYsjqSL-gzGzoHsz',
+  //appKey: 'rjsDBgksFXhcrW7mSXb5nOct',
+  appKey: 'T1SrPTGAxG2lSWQ8dht0W5Ph',
+});
